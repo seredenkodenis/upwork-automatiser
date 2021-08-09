4bba216853495f20de1def496dd378d250a8f5cd
Denis <denis-seredenko@ukr.net>
2021-08-09 16:44:42 +0300

    feature:#SC-10/changed layot + added hot reload value of slider + adapt

--- src/main/controllers/MovesController.java
+++ src/main/controllers/MovesController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
