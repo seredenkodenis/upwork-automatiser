fe5991d84b2dbfd81ee9a86977ff33f910c970e6
Cube_Bit <35814526+seredenkodenis@users.noreply.github.com>
2022-06-07 19:54:48 +0300

    Feature/sc 21/logback (#13)
    * feature:#SC-4/movement for exe generation
    
    * feature:#SC-7/created icon of applicaton
    
    * feature:#SC-8/added icon and title in top
    
    * feature:#SC-9/check close action for step dialog
    
    * feature:#SC-6/adaptive main window
    
    * feature:#SC-10/changed layot + added hot reload value of slider + adapt
    
    * bug-fix:SC-14/ user could bind more than 2 keys
    
    * feature:#SC-23/created anno for getting from properites
    
    * feature:#SC-15/small refactoring + button for rebinding
    
    * feature:#SC-16/better UI for screenshot
    
    * feature:#SC-17/hide windows before screenshot made
    
    * feature: hiding -> opacity
    
    This type because small amount of code, perfomance is better, hide() at javafx equals exit
    
    * feature:#SC-21/logback
    
    Co-authored-by: Denis <denis-seredenko@ukr.net>

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,12 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URL;
 
 public class Main extends Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("Start of logging");
         launch(args);
     }
 
--- src/main/dialog/CreateStepController.java
+++ src/main/dialog/CreateStepController.java
@@ -1,71 +1,258 @@
-package main.controllers;
+package main.dialog;
 
+import com.sun.javafx.robot.impl.FXRobotHelper;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.stage.FileChooser;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import main.annotations.Value;
+import main.data.ActionType;
 import main.data.Step;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class WelcomeSceneController {
+
+public class CreateStepController implements Initializable {
+
+    private final List<Integer> actions = new ArrayList<>();
 
     @FXML
-    private Button createButton;
+    private Button submitButton;
 
     @FXML
-    private void openButtonClicked() throws IOException, ClassNotFoundException {
-        List<Step> stepList = null;
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/resources/allMoves.fxml"));
+    private Label bindedButtons;
 
-        Stage currentStage = (Stage) createButton.getScene().getWindow();
+    @FXML
+    private Button bindButton;
 
-        FileChooser fileChooser = new FileChooser();
-        File file = fileChooser.showOpenDialog(currentStage);
-        if (file != null) {
-            stepList = openSaveFile(file);
+    @FXML
+    private CheckBox doubleClick;
+
+    @FXML
+    private CheckBox clickedStep;
+
+    @FXML
+    private CheckBox specialKeys;
+
+    @FXML
+    private TextField descriptionField;
+
+    @FXML
+    private Button pictureButton;
+
+    @FXML
+    private Button rebindButton;
+
+    @FXML
+    private ChoiceBox<Integer> sleepProp;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        sleepProp.getItems().removeAll(sleepProp.getItems());
+        sleepProp.getItems().addAll(0, 1, 2, 3, 5, 7, 10, 13, 15, 20, 30);
+        sleepProp.getSelectionModel().select(0);
+        pictureButton.setDisable(true);
+    }
+
+    private int tries = 2;
+
+    private Double pointX;
+
+    private Double pointY;
+
+    private Step step;
+
+    private ActionType type;
+
+    @Value(key = "dialog-title")
+    private String title;
+
+    private final EventHandler<KeyEvent> event = new EventHandler<KeyEvent>() {
+        @Override
+        public void handle(KeyEvent event) {
+            if (tries == 1) {
+                rebindButton.setDisable(false);
+                descriptionField.setDisable(false);
+                doubleClick.setDisable(true);
+                pictureButton.setDisable(true);
+                clickedStep.setDisable(true);
+                submitButton.setDisable(false);
+                specialKeys.setDisable(true);
+                bindButton.getScene().removeEventHandler(KeyEvent.KEY_PRESSED, this);
+            }
+
+            bindedButtons.setText(bindedButtons.getText() + " " + event.getCode().getName());
+            actions.add(event.getCode().impl_getCode());
+            tries = tries - 1;
+        }
+    };
+
+    @FXML
+    private void saveStep() {
+        if (!actions.isEmpty() || (pointX != null && pointY != null)) {
+            step = new Step();
+            if (pointX != null && pointY != null){
+                step.setPoint(new main.data.Point(pointX, pointY));
+            }
+            step.setActions(actions);
+            step.setType(type);
+            step.setDescription(descriptionField.getText());
+            step.setSleep(sleepProp.getValue());
         }
 
-        MovesController movesController = new MovesController();
-        movesController.setStepList(stepList);
-        loader.setController(movesController);
-
-        Parent parent = loader.load();
-
-        currentStage.setScene(new Scene(parent));
-        currentStage.show();
+        Stage currentStage = (Stage) bindedButtons.getScene().getWindow();
+        currentStage.close();
     }
 
     @FXML
-    private void newButtonClicked() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/resources/allMoves.fxml"));
-
-        MovesController movesController = new MovesController();
-        loader.setController(movesController);
-
-        Parent parent = loader.load();
-
-        Stage currentStage = (Stage) createButton.getScene().getWindow();
-        currentStage.setScene(new Scene(parent));
-        currentStage.show();
+    private void rebindButtons() {
+        tries = 2;
+        actions.clear();
+        bindedButtons.setText("");
+        rebindButton.setDisable(true);
+        bindButton.setDisable(false);
     }
 
-    private List<Step> openSaveFile(File file) throws IOException, ClassNotFoundException {
-        FileInputStream fileInputStream = new FileInputStream(file);
-        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-
-        ArrayList<Step> p2 = (ArrayList<Step>) objectInputStream.readObject();
-
-        objectInputStream.close();
-
-        return p2;
+    @FXML
+    private void isClicked() {
+        if (clickedStep.isSelected()) {
+            type = ActionType.CLICK;
+            specialKeys.setDisable(true);
+            doubleClick.setDisable(true);
+            bindButton.setDisable(true);
+            pictureButton.setDisable(false);
+        } else {
+            type = null;
+            specialKeys.setDisable(false);
+            doubleClick.setDisable(false);
+            bindButton.setDisable(false);
+            pictureButton.setDisable(true);
+        }
     }
+
+    @FXML
+    private void useSpecialKeys() {
+        if (specialKeys.isSelected()) {
+            doubleClick.setDisable(true);
+            pictureButton.setDisable(true);
+            clickedStep.setDisable(true);
+            bindButton.setDisable(false);
+            specialKeys.setDisable(true);
+        } else {
+            doubleClick.setDisable(false);
+            pictureButton.setDisable(false);
+            clickedStep.setDisable(false);
+            bindButton.setDisable(true);
+        }
+    }
+
+    @FXML
+    private void bindButtons() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setHeaderText("How to use this button");
+        alert.setContentText("After you understand press UNDERSTAND. Then press first key, then other.");
+
+        alert.showAndWait();
+
+        bindButton.getScene().addEventHandler(KeyEvent.KEY_PRESSED, event);
+        bindButton.setDisable(true);
+        descriptionField.setDisable(true);
+        submitButton.setDisable(true);
+        specialKeys.setDisable(true);
+        specialKeys.setSelected(true);
+    }
+
+    @FXML
+    private void isDoubleClicked() {
+        if (doubleClick.isSelected()) {
+            type = ActionType.CLICK;
+            specialKeys.setDisable(true);
+            clickedStep.setDisable(true);
+            bindButton.setDisable(true);
+            pictureButton.setDisable(false);
+        } else {
+            type = null;
+            specialKeys.setDisable(false);
+            clickedStep.setDisable(false);
+            bindButton.setDisable(false);
+            pictureButton.setDisable(true);
+        }
+    }
+
+    @FXML
+    public void makePicture() throws AWTException {
+
+        List<Stage> stages = FXRobotHelper.getStages();
+        stages.forEach(stage -> stage.setOpacity(0f));
+
+        Robot robot = new Robot();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().
+                getScreenSize();
+        final BufferedImage screen = robot.createScreenCapture(
+                new Rectangle(screenSize));
+
+
+        stages.forEach(stage -> stage.setOpacity(1f));
+
+        SwingUtilities.invokeLater(() -> {
+            JLabel screenLabel = new JLabel(new ImageIcon(screen));
+            JScrollPane screenScroll = new JScrollPane(screenLabel);
+            screenScroll.setPreferredSize(new Dimension(
+                    (int) (screenSize.getWidth() * 0.75),
+                    (int) (screenSize.getHeight() * 0.75)));
+
+            final Point pointOfInterest = new Point();
+
+            JPanel panel = new JPanel(new BorderLayout());
+            panel.add(screenScroll, BorderLayout.CENTER);
+
+            final JLabel pointLabel = new JLabel("Click on any point in the screen shot!");
+            panel.add(pointLabel, BorderLayout.SOUTH);
+
+            screenLabel.addMouseListener(new MouseAdapter() {
+
+                @Override
+                public void mouseClicked(MouseEvent me) {
+                    pointOfInterest.setLocation(me.getPoint());
+
+                    for (int i = pointOfInterest.x - 7; i < pointOfInterest.x + 7; ++i) {
+                        for (int j = pointOfInterest.y - 7; j < pointOfInterest.y + 7; ++j) {
+                            screen.setRGB(i, j, Color.RED.getRGB());
+                        }
+                    }
+
+                    screenLabel.repaint();
+                    pointLabel.setFont(new Font("Roboto", 50, 30));
+                    pointLabel.setText("Point: " + pointOfInterest.getX() + "x" + pointOfInterest.getY());
+                }
+            });
+
+            JOptionPane.showMessageDialog(null, panel);
+
+            pointX = pointOfInterest.getX();
+            pointY = pointOfInterest.getY();
+        });
+    }
+    //TODO: add here field with previous description
+
+    public Step getStep() {
+        return step;
+    }
+
 }
