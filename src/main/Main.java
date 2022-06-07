2cd2f9cdd562f371a84ee0f990e4760f18ed1267
Cube_Bit <35814526+seredenkodenis@users.noreply.github.com>
2022-06-07 19:40:37 +0300

    Feature/sc 8/added icon and title in top (#4)
    * feature:#SC-4/movement for exe generation
    
    * feature:#SC-7/created icon of applicaton
    
    * feature:#SC-8/added icon and title in top
    
    Co-authored-by: Denis <denis-seredenko@ukr.net>

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -21,6 +22,9 @@ public class Main extends Application {
         loader.setLocation(xmlUrl);
 
         Parent root = loader.load();
+        primaryStage.getIcons()
+                .add(new Image(getClass().getResourceAsStream("/main/resources/robot.png")));
+        primaryStage.setTitle("Screen cheater");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
