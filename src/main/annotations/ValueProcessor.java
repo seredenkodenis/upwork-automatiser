13704a162b61a6b4afc5685129399ba136eea464
Cube_Bit <35814526+seredenkodenis@users.noreply.github.com>
2022-06-07 19:49:13 +0300

    Feature/sc 23/own value anno (#9)
    * feature:#SC-4/movement for exe generation
    
    * feature:#SC-7/created icon of applicaton
    
    * feature:#SC-8/added icon and title in top
    
    * feature:#SC-9/check close action for step dialog
    
    * feature:#SC-6/adaptive main window
    
    * feature:#SC-10/changed layot + added hot reload value of slider + adapt
    
    * bug-fix:SC-14/ user could bind more than 2 keys
    
    * feature:#SC-23/created anno for getting from properites
    
    Co-authored-by: Denis <denis-seredenko@ukr.net>

---
+++ src/main/annotations/ValueProcessor.java
@@ -0,0 +1,55 @@
+package main.annotations;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValueProcessor {
+
+    public static void fillValues(Object object) throws IllegalAccessException {
+        Class<?> clazz = object.getClass();
+        Map<String, Field> fields = serialize(clazz);
+        setValues(fields, object);
+    }
+
+    private static Map<String, Field> serialize(Class<?> clazz) {
+        Map<String, Field> result = new HashMap<>();
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            Value annotation = field.getAnnotation(Value.class);
+            if (annotation != null) {
+                field.setAccessible(true);
+                result.put(annotation.key(), field);
+            }
+        }
+        return result;
+    }
+
+    private static void setValues(Map<String, Field> fields, Object object) throws IllegalAccessException {
+        Yaml yaml = new Yaml();
+
+        InputStream inputStream = ValueProcessor.class
+                .getResourceAsStream("/main/resources/messages/messages_en.yml");
+
+        Map<String, Object> parsed = yaml.load(inputStream);
+
+        //TODO:maybe make Context add this initialization at start of appplication
+
+        for (Map.Entry<String, Field> field : fields.entrySet()){
+            Object data = parsed.get(field.getKey());
+
+            if (data == null) {
+                throw new RuntimeException("Field with such key was not found! " + field.getValue().toGenericString());
+            }
+
+            field.getValue().setAccessible(true);
+            field.getValue().set(object, data);
+        }
+    }
+
+}
