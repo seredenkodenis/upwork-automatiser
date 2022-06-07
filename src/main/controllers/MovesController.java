1fb7ea52f91784f8c079e4bf801a7700bca2e8ff
Cube_Bit <35814526+seredenkodenis@users.noreply.github.com>
2022-06-07 19:46:26 +0300

    Feature/sc 10/adaptive bottom (#7)
    * feature:#SC-4/movement for exe generation
    
    * feature:#SC-7/created icon of applicaton
    
    * feature:#SC-8/added icon and title in top
    
    * feature:#SC-9/check close action for step dialog
    
    * feature:#SC-6/adaptive main window
    
    * feature:#SC-10/changed layot + added hot reload value of slider + adapt
    
    Co-authored-by: Denis <denis-seredenko@ukr.net>

--- src/main/controllers/MovesController.java
+++ src/main/controllers/MovesController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -48,7 +49,6 @@ public class MovesController {
         stage.setTitle("Create new step");
         stage.setScene(scene);
         stage.showAndWait();
-
 
         if (newStep.getStep() != null) {
             stepList.add(newStep.getStep());
